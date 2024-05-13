6b91c2fcc5e03e56a5a9e25876d413b498f49c37
belltown <yrw894fk256@proton.me>
2024-05-13 15:57:52 -0700

    deploy

--- src/lib/components/PlaylistTracksContent.svelte
+++ src/lib/components/PlaylistTracksContent.svelte
@@ -156,7 +156,7 @@
                         <p>{track.name} &#9702; {track.artist} &#9702; {Math.round(track.bpm)} bpm</p>
                     {/if}
                 </a>
-                <a style:color={track.previewUrl ? 'yellow' : 'red'} href={track.previewUrl}>
+                <a style:color={track.previewUrl !== 'null' ? 'yellow' : 'red'} href={track.previewUrl}>
                     Preview
                 </a>
             </li>
